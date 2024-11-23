134600eadef8e66d85392ec914587db91240e1c5
Shan Ali <shan.softwareengineer007@gmail.com>
2024-11-23 11:14:08 -0800

    bbbbbb

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Default Next.js configuration
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
