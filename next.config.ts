9526efd2f65380f9563d5fc92be786ac53af6134
Shan Ali <shan.softwareengineer007@gmail.com>
2024-11-23 09:30:33 -0800

    okk

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Place your config options here
-  swcMinify: true, // Use SWC minifier (optional)
-  eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint during production builds (optional)
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
